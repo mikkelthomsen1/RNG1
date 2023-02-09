c9e116cc4c53e911a1d1a809e867252c4528f609
mikkelthomsen1 <mikkel.thomsen18@gmail.com>
2023-02-09 15:13:18 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        rng(0100);
+    }
+
+    public static void rng(int input) {
+        int startinput = input;
+        int count = 0;
+        int tæller = 0;
+
+        do {
+            input = input * input;
+            while (input < 1000000) {
+                input = input * 10;
+                tæller++;
+            }
+            input = (int) ((input / 100) % (10000 * Math.pow(10,tæller)));
+           // input = (input / 100) % 10000;
+            count++;
+            System.out.println(input);
+        } while (input != startinput);
+        System.out.println(count);
     }
 }
